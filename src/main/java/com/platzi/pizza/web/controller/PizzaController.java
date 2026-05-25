40b6422fd7f04fe211f8b6a7e25da1839552cae0
johncortes8906 <johncortes8906@gmail.com>
2026-05-25 15:20:17 -0500

    Apply request matches rules

--- src/main/java/com/platzi/pizza/web/controller/PizzaController.java
+++ src/main/java/com/platzi/pizza/web/controller/PizzaController.java
@@ -57,6 +57,7 @@ public class PizzaController {
     }
 
     @GetMapping("/available")
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<Page<PizzaEntity>> getAllAvailable(@RequestParam(defaultValue = "0") int page,
                                                              @RequestParam(defaultValue = "8") int elements,
                                                              @RequestParam(defaultValue = "price") String sortBy,
